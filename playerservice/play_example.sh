fd56879799da920676ce27a795baa780208c047a
mml1975 <maslm7@gmail.com>
2026-02-07 17:00:42 +0300

    Add files via upload

---
+++ playerservice/play_example.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:5000/play \
+  -H "Content-Type: application/json" \
+  -d '{"file_path": "/путь/к/вашему/файлу.mp3"}'
---
+++ playerservice/volume.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:5000/volume \
+  -H "Content-Type: application/json" \
+  -d '{"volume": 0.5}'
